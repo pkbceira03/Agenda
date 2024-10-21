cd9afecf6d9a9ad139ee854db104dbfa11a258f4
pkbceira03 <cabeceira2003@gmail.com>
2024-10-21 17:51:22 -0300

    cria tabela de eventos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from core.models import Eventos
 
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_evento', 'data_criacao')
+    list_filter = ('titulo',)
+
+admin.site.register(Eventos, EventoAdmin)
